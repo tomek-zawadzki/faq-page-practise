f7e4fdc4baf418db42be226b1b8a1ada3d4ebec0
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-03 11:47:10 +0100

    trying to change function to use toggel method, not everythig works perfect, later i will fix it

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
+const toggle = document.querySelectorAll(".question__div");
 const answer = document.querySelectorAll(".question__answer");
-const plus = document.querySelectorAll(".plus");
-const minus = document.querySelectorAll(".minus");
+const sentence = document.querySelectorAll(".question__sentence");
+// const plus = document.querySelectorAll(".plus");
+// const minus = document.querySelectorAll(".minus");
 
-plus.forEach((e, i) => {
+// plus.forEach((e, i) => {
+//   e.addEventListener("click", () => {
+//     answer[i].classList.remove("hidden");
+//     plus[i].classList.add("hidden");
+//     minus[i].classList.remove("hidden");
+//   });
+// });
+
+// minus.forEach((e, i) => {
+//   e.addEventListener("click", () => {
+//     answer[i].classList.add("hidden");
+//     plus[i].classList.remove("hidden");
+//     minus[i].classList.add("hidden");
+//   });
+// });
+
+// for (let i = 0; i < answer.length; i++) {
+//   answer[i].addEventListener("click", () => {
+//     // this.clssList.toggle("active");
+//     console.log("ok");
+//   });
+// }
+
+toggle.forEach((e, i) => {
   e.addEventListener("click", () => {
-    answer[i].classList.remove("hidden");
-    plus[i].classList.add("hidden");
-    minus[i].classList.remove("hidden");
+    answer[i].classList.toggle("active");
+    sentence[i].classList.toggle("active");
   });
 });
-
-minus.forEach((e, i) => {
-  e.addEventListener("click", () => {
-    answer[i].classList.add("hidden");
-    plus[i].classList.remove("hidden");
-    minus[i].classList.add("hidden");
-  });
-});
-
